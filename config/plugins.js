dd0304e1c983fdc3d183efa8cb3925a77cf999a6
Dabo-2100 <a_fattah_m@icloud.com>
2024-06-19 21:03:18 +0300

    Dabo

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,12 @@
 module.exports = () => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_bdFmbmfQdUQNjfI3TZs7q4hiDt5J/nRpnv0yCXz",
+      apiToken: "k54N4mLDdVLu5kPO5YPWy1xL",
+    },
   },
 });
+
+// Token : k54N4mLDdVLu5kPO5YPWy1xL
